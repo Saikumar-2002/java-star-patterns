a2241db80a7bcf6be27ecbb473604e77a3ea570d
sai kumar <saikumarkalva10@gmail.com>
2024-10-18 23:46:55 +0530

    second message

---
+++ src/NumberIncreasePyramid.java
@@ -0,0 +1,15 @@
+public class NumberIncreasePyramid {
+    public static void main(String[] args) {
+        int n=6;
+        pattern2(n);
+    }
+
+    public static void pattern2(int n){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/squarePattern.java
+++ src/squarePattern.java
@@ -6,7 +6,7 @@ public  class squarePattern {
 
     public static void pattern1(int n){
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+            for(int j=0;j<n;j++){
                 System.out.print("* ");
             }
             System.out.println();
